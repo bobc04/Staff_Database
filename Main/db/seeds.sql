09d61656d57b397e24ad2acd86185046deea2c53
bobc04 <rcoccaro@duck.com>
2025-01-08 18:02:15 -0500

    schema and seeds sql files configured

---
+++ Main/db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO deparment (deparment)
+VALUES ('Delta Force'),
+       ('The High Table'),
+       ('Villa Riders'),
+       ('Heartbreak Ridge'),
+       ('Avengers'),
+       ('Avengers'),
+       ('Avengers'),
+       ('Avengers'), 
+
+INSERT INTO role (job_role)
+VALUES ('Street Fighter'),
+       ('Mercenary'),
+       ('Machine Gunner'),
+       ('Dirty Harry'),
+       ('Avenger');
+       ('Avenger');
+       ('Avenger');
+       ('Avenger');
+
+INSERT INTO employee (employee_name)
+VALUES ('Chuck Norris'),
+       ('John Wick'),
+       ('Charles Bronson'),
+       ('Clint Eastwood'),
+       ('Captain America');
+       ('Spiderman');
+       ('Thor');
+       ('Tony Stark');
