e7e959e7235a0e75960004308da045a01d4fd607
bobc04 <rcoccaro@duck.com>
2025-01-09 17:02:01 -0500

    Completed

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -3,28 +3,25 @@ CREATE DATABASE employee_db;
 
 \c employee_db;
 
-CREATE TABLE deparment (
+CREATE TABLE department (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(100) NOT NULL
+  department VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE role (
-    id SERIAL PRIMARY KEY,
-    title TEXT NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id TEXT NOT NULL
-    FOREIGN KEY (employee_db)
-    REFERENCES employee (id)
-    ON DELETE SET NULL
+  id SERIAL PRIMARY KEY,
+  title TEXT NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER
-    FOREIGN KEY (employee_db)
-    REFERENCES employee (id)
-    ON DELETE SET NULL
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER,
+  FOREIGN KEY (role_id) REFERENCES role (id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE SET NULL
 );
--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -1,29 +1,23 @@
-INSERT INTO deparment (deparment)
-VALUES ('Delta Force'),
-       ('The High Table'),
-       ('Villa Riders'),
-       ('Heartbreak Ridge'),
-       ('Avengers'),
-       ('Avengers'),
-       ('Avengers'),
-       ('Avengers'), 
+INSERT INTO department (department)
+VALUES 
+  ('Delta Force'),
+  ('The High Table'),
+  ('Villa Riders'),
+  ('Heartbreak Ridge'),
+  ('Avengers');
 
-INSERT INTO role (job_role)
-VALUES ('Street Fighter'),
-       ('Mercenary'),
-       ('Machine Gunner'),
-       ('Dirty Harry'),
-       ('Avenger');
-       ('Avenger');
-       ('Avenger');
-       ('Avenger');
+INSERT INTO role (title, salary, department_id)
+VALUES 
+  ('Street Fighter', 50000, 1),
+  ('Mercenary', 60000, 2),
+  ('Machine Gunner', 70000, 3),
+  ('Dirty Harry', 80000, 4),
+  ('Avenger', 90000, 5);
 
-INSERT INTO employee (employee_name)
-VALUES ('Chuck Norris'),
-       ('John Wick'),
-       ('Charles Bronson'),
-       ('Clint Eastwood'),
-       ('Captain America');
-       ('Spiderman');
-       ('Thor');
-       ('Tony Stark');
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+  ('Chuck', 'Norris', 1, 1),
+  ('John', 'Wick', 2, 2),
+  ('Charles', 'Bronson', 3, 3),
+  ('Clint', 'Eastwood', 4, 4),
+  ('Steve', 'Rogers', 5, 5);
